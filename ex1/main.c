aa3421f72b7c5ba5181b3835299159c5b78f0b90
Belle <79115662+Isabelle-Matos@users.noreply.github.com>
2023-04-26 15:02:08 -0300

    update

--- ex1/main.c
+++ ex1/main.c
@@ -3,21 +3,24 @@
 
 int main(){
 
-    polinomio *p1;
-    Lista l1;
+    polinomio p1, p2;
+    Lista l1, l2;
     cria_lista(&l1);
+    cria_lista(&l2);
 
 
     printf("%d\n", vazia(l1));
+    printf("%d\n", vazia(l2));
+ 
  
 
-    p1->expoente = 2;
-    p1->numero = 3;
-    insere(*p1, l1);
+    p1.expoente = 2;
+    p1.numero = 3;
+    insere(p1, l1);
     
-    p1->expoente = 2;
-    p1->numero = 1;
-    insere(*p1, l1);
+    p2.expoente = 2;
+    p2.numero = 1;
+    insere(p2, l1);
    
 
 
